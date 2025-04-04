09345c1d6c58aef3ae14ced828158cf7c7ea3151
hieusStorm <hyrum555@gmail.com>
2025-04-04 10:07:26 -0700

    Added Validation for weapons & updated valdation for monster to check against a list of potential elements

--- middleware/validate.js
+++ middleware/validate.js
@@ -1,9 +1,76 @@
 const validator = require("../helpers/validate");
 
 const saveMonster = (req, res, next) => {
+    const elements = ['fire', 'water', 'thunder', 'ice'];
     const validationRule = {
         name: "required|string",
-        elementWeakness: "required|string",
+        elementWeakness: {inclusion: elements},
+        weakSpot: "required|string",
+        damageTypeWeak: "required|string",
+        type: "required|string"
+    };
+    validator(req.body, validationRule, {}, (err, status) => {
+        if(!status) {
+            res.status(412).send({
+                success: false,
+                message: "Validation Failed",
+                data: err
+            });
+        } else {
+            next();
+        }
+    });
+};
+const saveWeapon = (req, res, next) => {
+    const elementTypes = ['fire', 'water', 'thunder', 'ice', 'dragon', 'poison', 'paraylis', 'blast', 'sleep'];
+    const damageTypes = ['slash', 'impact', 'shot'];
+    const validationRule = {
+        name: "required|string",
+        elementType: {inclusion: elementTypes},
+        elementValue: "required|integer",
+        damageType: {inclusion: damageTypes},
+        attackValue: "required|integer",
+        weaponType: "required|string",
+    };
+    validator(req.body, validationRule, {}, (err, status) => {
+        if(!status) {
+            res.status(412).send({
+                success: false,
+                message: "Validation Failed",
+                data: err
+            });
+        } else {
+            next();
+        }
+    });
+};
+// Placeholders
+const saveBuild = (req, res, next) => {
+    const elements = ['fire', 'water', 'thunder', 'ice'];
+    const validationRule = {
+        name: "required|string",
+        elementWeakness: {inclusion: elements},
+        weakSpot: "required|string",
+        damageTypeWeak: "required|string",
+        type: "required|string"
+    };
+    validator(req.body, validationRule, {}, (err, status) => {
+        if(!status) {
+            res.status(412).send({
+                success: false,
+                message: "Validation Failed",
+                data: err
+            });
+        } else {
+            next();
+        }
+    });
+};
+const saveUser = (req, res, next) => {
+    const elements = ['fire', 'water', 'thunder', 'ice'];
+    const validationRule = {
+        name: "required|string",
+        elementWeakness: {inclusion: elements},
         weakSpot: "required|string",
         damageTypeWeak: "required|string",
         type: "required|string"
@@ -22,5 +89,8 @@ const saveMonster = (req, res, next) => {
 };
 
 module.exports = {
-    saveMonster
+    saveMonster,
+    saveWeapon,
+    saveBuild,
+    saveUser
 }
