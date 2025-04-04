1e77dc4b5f2ac25b93b06256d7dc3c1900b182bf
hieusStorm <hyrum555@gmail.com>
2025-04-04 09:24:08 -0700

    Unified qoutes

--- controllers/monsters.js
+++ controllers/monsters.js
@@ -1,5 +1,5 @@
-const mongodb = require("../data/database");
-const ObjectId = require("mongodb").ObjectId;
+const mongodb = require('../data/database');
+const ObjectId = require('mongodb').ObjectId;
 
 // functions
 const getAll = async (req, res) => {
@@ -14,12 +14,12 @@ const getAll = async (req, res) => {
 
 const getSingle = async (req, res) => {
     if(!ObjectId.isValid(req.params.id)) {
-        res.status(400).json("Must us a valid monster ID to find");
+        res.status(400).json('Must us a valid monster ID to find');
     }
     const monsterId = new ObjectId(req.params.id);
-    const result = await mongodb.getDb().db().collection("monster").find({_id: monsterId});
+    const result = await mongodb.getDb().db().collection('monster').find({_id: monsterId});
     result.toArray().then(monster => {
-        res.setHeader("Content-Type", "application/json");
+        res.setHeader('Content-Type', 'application/json');
         res.status(200).json(monster[0]);
     }).catch((err)=> res.status(400).json({message: err}));
 };
@@ -33,18 +33,18 @@ const createMonster = async (req, res) => {
         type: req.body.type
     };
 
-    const response = await mongodb.getDb().db().collection("monster").insertOne(monster);
+    const response = await mongodb.getDb().db().collection('monster').insertOne(monster);
 
     if(response.acknowledged) {
         res.status(204).send();
     } else {
-        res.status(500).json(response.error || "An error occurred while adding a monster");
+        res.status(500).json(response.error || 'An error occurred while adding a monster');
     }
 };
 
 const updateMonster = async (req, res) => {
     if(!ObjectId.isValid(req.params.id)) {
-        res.status(400).json("Must use a valid monster Id to update");
+        res.status(400).json('Must use a valid monster Id to update');
     } 
 
     const monsterId = new ObjectId(req.params.id);
@@ -57,23 +57,23 @@ const updateMonster = async (req, res) => {
         type: req.body.type
     };
 
-    const response = await mongodb.getDb().db().collection("monster").replaceOne({_id: monsterId}, monster);
+    const response = await mongodb.getDb().db().collection('monster').replaceOne({_id: monsterId}, monster);
 
     if(response.modifiedCount > 0) {
         res.status(204).send();
     } else {
-        res.status(500).json(response.error || "An Error occured while updating the monster");
+        res.status(500).json(response.error || 'An Error occured while updating the monster');
     }
 };
 
 const deleteMonster = async (req, res) => {
     if(!ObjectId.isValid(req.params.id)) {
-        res.status(400).json("Must use a valid monster ID to delete.");
+        res.status(400).json('Must use a valid monster ID to delete.');
     }
 
     const monsterId = new ObjectId(req.params.id);
     
-    const response = await mongodb.getDb().db().collection("monster").deleteOne({_id: monsterId});
+    const response = await mongodb.getDb().db().collection('monster').deleteOne({_id: monsterId});
 
     if(response.deletedCount > 0) {
         res.status(204).send();
